c4e3d469d46afe3b15c3c840fe337e06e6e84a70
Melvic Gomez <melvicgomez.ph@gmail.com>
2020-02-20 11:02:58 +0800

    Remove unnecessary files and codes.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,21 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { ThemeProvider } from "styled-components"
-import { useStaticQuery, graphql } from "gatsby"
+import styled, { ThemeProvider } from "styled-components"
 
-import Header from "./header"
 import "./layout.css"
 
-const theme = {
-  main: "mediumseagreen"
-};
+const AppContainer = styled.div``
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+/**
+ * theme will be acccessible on the styled components props
+ */
+const theme = {}
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </ThemeProvider>
-  )
-}
+const Layout = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <AppContainer>{children}</AppContainer>
+  </ThemeProvider>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
