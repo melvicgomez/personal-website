63c5153a4bdf8c55c659b821dbaf086918c9bda6
Melvic Gomez <melvicgomez.ph@gmail.com>
2020-04-08 15:19:55 +0800

    Added page not found.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,51 @@
 import React from "react"
-
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import styled from "styled-components"
+import { Button, Icon, Row, Col } from "antd"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const StyledNotFoundPage = styled.div`
+  height: 100vh;
+  display: flex;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+`
+
+function NotFoundPage() {
+  return (
+    <Layout>
+      <StyledNotFoundPage>
+        <Row type="flex" justify="center" align="middle">
+          <Col
+            xs={{ span: 23 }}
+            sm={{ span: 23 }}
+            md={{ span: 23 }}
+            lg={{ span: 12 }}
+            xl={{ span: 12 }}
+            xxl={{ span: 10 }}
+          >
+            <img
+              src={require("../images/undraw-page-not-found.png")}
+              alt="Page not found placeholder."
+            />
+            <br />
+            <div style={{ textAlign: "center" }}>
+              <p>
+                You hit a page does not exist in my website. Try to click the
+                link to see what I have in this website.
+              </p>
+              <Button type="link" href="/">
+                <Icon type="arrow-left" />
+                Return to home page
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </StyledNotFoundPage>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
